685e0d0297e824a936bb9742f64a3a51304f7c4e
Daniel Díaz <daniel0diiaz@gmail.com>
2022-03-15 19:06:19 +0100

    issue #8 añadidas relaciones con inventor y patron #2
    Añadidas restricciones a campos de la entidad

--- src/main/java/acme/entities/patronage/Patronage.java
+++ src/main/java/acme/entities/patronage/Patronage.java
@@ -1,13 +1,22 @@
 package acme.entities.patronage;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
 
 import acme.enums.Status;
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
+import acme.roles.Patron;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,12 +27,21 @@ public class Patronage extends AbstractEntity {
 
 	protected static final long serialVersionUID = 1L;
 	
+	public Inventor inventor;
+	
+	public Patron patron;
+	
 	public Status status;
 	
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	@Column(unique=true)
 	public String code;
 	
+	@NotBlank
+	@Size(max = 255)
 	public String legalStuff;
 	 
+	@Min(value = 0L)
 	public Float budget;
 	
 	@Temporal(TemporalType.TIMESTAMP)
@@ -33,5 +51,8 @@ public class Patronage extends AbstractEntity {
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
 	public Date finishesAt;
+	
+	@URL
+	protected String link;
 	
 }
