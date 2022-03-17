8767652c3a0c04edb5cba866c95dd8f3d2640c6c
luibelzan <luismi_madrilista@hotmail.com>
2022-03-17 20:37:20 +0100

    System configuration added

--- src/main/java/acme/entities/configuration/Configuration.java
+++ src/main/java/acme/entities/configuration/Configuration.java
@@ -31,5 +31,60 @@ private static final long	serialVersionUID	= 1L;
 	@NotNull
 	@Range(min = 0, max = 1)
 	private Double weakSpamThreshold;
+	
+	
+	public boolean isSpamWeak(final String text) {
+		final String[] lowerCaseText = text.toLowerCase().replaceAll("\\s+", " ").split(" ");
+		int spamCount = 0;
+		final String[] sp = this.weakSpamWords.split(",");
+		
+		for (final String s : sp) {
+			if (text.toLowerCase().trim().replaceAll("\\s+", " ").contains(s)) {
+				spamCount++;
+			}
+			for (int i = 0; i < lowerCaseText.length; i++) {
+				if (lowerCaseText[i].contains(s)) {
+					spamCount++;
+				}
+
+			}
+		}
+		if (spamCount % 2 == 0) {
+			spamCount = spamCount / 2;
+		} else {
+			spamCount = (spamCount / 2) + 1;
+		}
+		final Double umbral = (double) spamCount / lowerCaseText.length;
+
+		return umbral > this.weakSpamThreshold;
+
+	}
+	
+	public boolean isSpamStrong(final String text) {
+		final String[] lowerCaseText = text.toLowerCase().replaceAll("\\s+", " ").split(" ");
+		int spamCount = 0;
+		final String[] sp = this.strongSpamWords.split(",");
+		
+		for (final String s : sp) {
+			if (text.toLowerCase().trim().replaceAll("\\s+", " ").contains(s)) {
+				spamCount++;
+			}
+			for (int i = 0; i < lowerCaseText.length; i++) {
+				if (lowerCaseText[i].contains(s)) {
+					spamCount++;
+				}
+
+			}
+		}
+		if (spamCount % 2 == 0) {
+			spamCount = spamCount / 2;
+		} else {
+			spamCount = (spamCount / 2) + 1;
+		}
+		final Double umbral = (double) spamCount / lowerCaseText.length;
+
+		return umbral > this.strongSpamThreshold;
+
+	}
 
 }
